ce8a36f30755d093aa6e0fd5d3ffff9cf4e5b083
Denis Olifer <denis@olifer.org>
2020-05-19 00:09:22 +0300

    + Added Projects and About pages
    * fixed nav bar style for active items

--- src/Blog.Generator/Program.cs
+++ src/Blog.Generator/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
 using Statiq.App;
 using Statiq.Common;
 using Statiq.Web;
@@ -11,9 +13,22 @@ namespace Blog.Generator
         {
             return await Bootstrapper.Factory
                 .CreateWeb(args)
-                .AddSetting(Keys.Host, "olifer.org")
+                .AddSetting(Keys.Host, new Uri(BlogSettings.ServerUri).Host)
                 .AddSetting(Keys.LinksUseHttps, true)
                 .AddSetting(WebKeys.MirrorResources, true)
+                .AddSetting(
+                    Keys.DestinationPath,
+                    Config.FromDocument((doc, ctx) =>
+                    {
+                        // Only applies to the content pipeline
+                        if (ctx.PipelineName == nameof(Statiq.Web.Pipelines.Content))
+                        {
+                            return doc.Source.Parent.Segments.Last().SequenceEqual("posts".AsMemory())
+                                ? new NormalizedPath(BlogSettings.BlogPath).Combine(doc.GetDateTime(WebKeys.Published).ToString("yyyy")).Combine(doc.GetString("Category")).Combine(doc.Destination.FileName.ChangeExtension(".html"))
+                                : doc.Destination.ChangeExtension(".html");
+                        }
+                        return doc.Destination;
+                    }))
                 .RunAsync();
         }
     }

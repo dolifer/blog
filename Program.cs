31f5bb28729dd661c0ace8be9f2eaf99c1b1aab9
Denis Olifer <denis@olifer.org>
2020-06-21 14:56:30 +0300

    Try to use github pages

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 using System.Threading.Tasks;
 using Statiq.App;
 using Statiq.Common;
@@ -16,19 +15,8 @@ namespace Blog.Generator
                 .AddSetting(Keys.Host, new Uri(BlogSettings.ServerUri).Host)
                 .AddSetting(Keys.LinksUseHttps, true)
                 .AddSetting(WebKeys.MirrorResources, true)
-                // .AddSetting(
-                //     Keys.DestinationPath,
-                //     Config.FromDocument((doc, ctx) =>
-                //     {
-                //         // Only applies to the content pipeline
-                //         if (ctx.PipelineName == nameof(Statiq.Web.Pipelines.Content))
-                //         {
-                //             return doc.Source.Parent.Segments.Last().SequenceEqual("posts".AsMemory())
-                //                 ? new NormalizedPath(BlogSettings.BlogPath).Combine(doc.GetDateTime(WebKeys.Published).ToString("yyyy")).Combine(doc.GetString("Category")).Combine(doc.Destination.FileName.ChangeExtension(".html"))
-                //                 : doc.Destination.ChangeExtension(".html");
-                //         }
-                //         return doc.Destination;
-                //     }))
+                .DeployToGitHubPagesBranch("dolifer", "olifer.org", Config.FromSetting<string>("GITHUB_TOKEN"), "master")
+                .AddPipelines()
                 .RunAsync();
         }
     }

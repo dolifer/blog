16b0a79a14ef25317da4c304de6fe9cdc39167df
Denis Olifer <denis@olifer.org>
2020-06-23 21:10:02 +0300

    Fix header nav to not have a link to itself
    + added static class that holds navigation

---
+++ Data/Navigation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Blog.Generator.Models;
+using Statiq.Common;
+
+namespace Blog.Generator.Data
+{
+    public static class Navigation
+    {
+        public static IEnumerable<HeaderLink> GetHeaderLinks(IDocument document)
+        {
+            yield return new HeaderLink(document, "/", "Blog");
+            yield return new HeaderLink(document, "/projects", "Projects");
+            yield return new HeaderLink(document, "/about", "About");
+        }
+    }
+}
---
+++ Models/HeaderLink.cs
@@ -0,0 +1,18 @@
+﻿using Statiq.Common;
+
+namespace Blog.Generator.Models
+{
+    public class HeaderLink
+    {
+        public IDocument Document { get; }
+        public string Url { get; }
+        public string Name { get; }
+
+        public HeaderLink(IDocument document, string url, string name)
+        {
+            Document = document;
+            Url = url;
+            Name = name;
+        }
+    }
+}

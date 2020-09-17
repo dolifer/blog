e0ed473427cdc3d7840fecebcc85229caab0a4e4
Denis Olifer <denis@olifer.org>
2020-09-17 19:04:32 +0300

    Add GitVersion

---
+++ Data/GitVersion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection;
+
+namespace Blog.Generator.Data
+{
+    public static class GitVersion
+    {
+        private static readonly Type _gitVersionInformationType;
+
+        static GitVersion()
+        {
+            var assembly = Assembly.GetEntryAssembly();
+            _gitVersionInformationType = assembly?.GetType("GitVersionInformation");
+        }
+
+        public static object SemVer => GetField("FullSemVer");
+        public static object Sha => GetField("Sha");
+
+        private static object GetField(string name) => _gitVersionInformationType.GetField(name)?.GetValue(null);
+    }
+}

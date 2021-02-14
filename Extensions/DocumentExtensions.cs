2bafb9623f837d582a0c3bde98d758cb23075771
Denis Olifer <denis@olifer.org>
2021-02-14 11:57:17 +0200

    Updated feed settings

---
+++ Extensions/DocumentExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Statiq.Common;
+
+namespace Blog.Generator.Extensions
+{
+    public static class DocumentExtensions
+    {
+        public static string GetProperty(this IDocument document, string key, string fallbackKey = null)
+        {
+            var keyValue = document.GetString(key);
+
+            if (!string.IsNullOrWhiteSpace(keyValue))
+            {
+                return keyValue;
+            }
+
+            if (string.IsNullOrWhiteSpace(fallbackKey))
+            {
+                return null;
+            }
+
+            var fallbackValue = document.GetString(fallbackKey);
+
+            return !string.IsNullOrWhiteSpace(fallbackValue) ? fallbackValue : null;
+        }
+
+        public static string GetLeadImage(this IDocument document)
+        {
+            var image = document.GetString("Image");
+            var imageLink = document.GetString("ImageLink");
+
+            if (!string.IsNullOrWhiteSpace(image) && !string.IsNullOrWhiteSpace(imageLink))
+            {
+                return image;
+            }
+
+            return null;
+        }
+    }
+}
--- input/posts/quickly-showhide-hidden-files-mac-os-x.md
+++ input/posts/quickly-showhide-hidden-files-mac-os-x.md
@@ -1,7 +1,6 @@
 ---
 Title: The Quickest Way to Show/Hide Hidden Files in OS X
 Published: 2020-11-30
-Image: ""
 ---
 
 Since the release of macOS Sierra, when in Finder, it is now possible to use the shortcut: <kbd>CMD</kbd> + <kbd>SHIFT</kbd> + <kbd>.</kbd>
